2c23c0831451bec97165085bb39b3fde1f042547
Daniel Marques <dddaniel.marques24@gmail.com>
2025-09-18 21:40:04 -0300

    :sparkles: feat: add initial project files and structure

---
+++ integrador-mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Tabs } from 'expo-router';
+// A CORREÇÃO ESTÁ AQUI: trocamos '~/' por '@/':
+import CustomTabBar from '@/components/CustomTabBar'; 
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      tabBar={(props) => <CustomTabBar {...props} />}
+    >
+      <Tabs.Screen name="home" options={{ headerShown: false }} />
+      <Tabs.Screen name="ranking" options={{ headerShown: false }} />
+      <Tabs.Screen name="squads" options={{ headerShown: false }} />
+      <Tabs.Screen name="usuarios" options={{ headerShown: false }} />
+      <Tabs.Screen name="notificacoes" options={{ headerShown: false }} />
+    </Tabs>
+  );
+}
