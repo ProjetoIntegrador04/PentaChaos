f488781d1096fce1596fd950fff956b15e24887d
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2025-09-27 14:50:42 -0300

    :sparkles: feat: add initial project structure with Maven wrapper and Spring Boot setup

---
+++ backend/sgeapp/src/main/java/com/sge/sgeapp/sgeapp/SgeappApplication.java
@@ -0,0 +1,13 @@
+package com.sge.sgeapp.sgeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgeappApplication.class, args);
+	}
+
+}
---
+++ backend/sgeapp/src/test/java/com/sge/sgeapp/sgeapp/SgeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sge.sgeapp.sgeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
