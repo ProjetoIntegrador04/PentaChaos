856a9dc4e4e766ffa568b50872f337df75895807
Juan Pablo <juansilverio865@gmail.com>
2025-10-27 20:56:37 -0300

    feat(task): implementar lógica de CRUD em TaskService

--- backend/sge-app/src/main/java/com/sge/sge_app/services/TaskService.java
+++ backend/sge-app/src/main/java/com/sge/sge_app/services/TaskService.java
@@ -1,5 +1,47 @@
 package com.sge.sge_app.services;
 
+import com.sge.sge_app.models.Task;
+import com.sge.sge_app.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TaskService {
-    
+
+    private final TaskRepository repository;
+
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public Task createTask(Task task) {
+        return repository.save(task);
+    }
+
+    public Task updateTask(Long id, Task taskDetails) {
+        Task task = getTaskById(id);
+        if (task != null) {
+            task.setTitulo(taskDetails.getTitulo());
+            task.setDescricao(taskDetails.getDescricao());
+            task.setStatus(taskDetails.getStatus());
+            task.setPrioridade(taskDetails.getPrioridade());
+            task.setResponsavel(taskDetails.getResponsavel());
+            task.setDataConclusao(taskDetails.getDataConclusao());
+            return repository.save(task);
+        }
+        return null;
+    }
+
+    public void deleteTask(Long id) {
+        repository.deleteById(id);
+    }
 }
